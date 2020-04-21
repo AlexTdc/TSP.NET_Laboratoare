e4ff70e99071f648c97ebdcba97b17b27bf3f4c1
AlexTdc <48626305+AlexTdc@users.noreply.github.com>
2020-04-21 13:23:56 +0300

    Add files via upload

---
+++ Teodorescu_Alexandru_P2_Mi12/ObjectWCF/IMplementInterface.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API;
+
+namespace ObjectWCF
+{
+    public class Model : IMplementInterface
+    {
+        apiControlForm1 api1 = new apiControlForm1();
+        apiControlForm2 api2 = new apiControlForm2();
+
+        void InterfaceImages.deleteImage(string Path)
+        {
+            api2.deleteImage(Path);
+
+        }
+
+
+        void InterfaceImages.saveImage(string Name, string Path, string About, string Place, string People)
+        {
+            //apiControlForm1 api = new apiControlForm1();
+            api1.saveImage(Name, Path, About, Place, People);
+             
+        }
+
+        void InterfaceImages.UpdateImage1(string Name, string Path, string About, string Place, string People)
+        {
+            //apiControlForm2 api = new apiControlForm2();
+            api2.UpdateImage1(Name, Path, About, Place, People);
+        }
+    }
+
+}
