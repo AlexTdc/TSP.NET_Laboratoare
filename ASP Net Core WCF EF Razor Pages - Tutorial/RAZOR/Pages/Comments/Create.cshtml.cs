eddda91245e3f93bbcad555d0b924a71ca019e8b
AlexTdc <48626305+AlexTdc@users.noreply.github.com>
2020-05-13 13:20:04 +0300

    Add files via upload

---
+++ ASP Net Core WCF EF Razor Pages - Tutorial/RAZOR/Pages/Comments/Create.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RAZOR.Models;
+using ServiceReferencePostComment;
+namespace RAZOR
+{
+    public class CreateModel : PageModel
+    {
+        PostCommentClient pcc = new PostCommentClient();
+        public CreateModel()
+        {
+            CommentDTO = new CommentDTO();
+        }
+        [BindProperty]
+        public CommentDTO CommentDTO { get; set; }
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id.HasValue)
+            {
+                var itemPost = await pcc.GetPostByIdAsync(id.Value);
+                ViewData["id"] = id.Value.ToString() + " : " + itemPost.Description;
+                CommentDTO.PostPostId = id.Value;
+            }
+            return Page();
+        }
+    }
+}
