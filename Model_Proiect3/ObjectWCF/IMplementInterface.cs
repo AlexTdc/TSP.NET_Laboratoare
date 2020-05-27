df7e401b54488c0202e2ee991a50fc703e3f58b3
AlexTdc <48626305+AlexTdc@users.noreply.github.com>
2020-05-27 14:55:14 +0300

    Add files via upload

---
+++ Model_Proiect3/ObjectWCF/IMplementInterface.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API;
+using Model;
+
+namespace ObjectWCF
+{
+    public class Model : IMplementInterface
+    {
+        apiControlForm1 api1 = new apiControlForm1();
+        apiControlForm2 api2 = new apiControlForm2();
+
+        void InterfaceImages.deleteImage(string Path)
+        {
+            api2.deleteImage(Path);
+
+        }
+
+
+        void InterfaceImages.saveImage(string Name, string Path, string About, string Place, string People)
+        {
+            //apiControlForm1 api = new apiControlForm1();
+            api1.saveImage(Name, Path, About, Place, People);
+             
+        }
+
+        void InterfaceImages.UpdateImage1(string Name, string Path, string About, string Place, string People)
+        {
+            //apiControlForm2 api = new apiControlForm2();
+            api2.UpdateImage1(Name, Path, About, Place, People);
+        }
+
+        List<PhotosVideos> InterfaceImages.getPhotos()
+        {
+            return api2.getPhotos();
+            
+        }
+
+        List<Place> InterfaceImages.getPlaces()
+        {
+            return api2.getPlaces();
+
+        }
+
+        List<Person> InterfaceImages.getPerson()
+        {
+            return api2.getPerson();
+
+        }
+    }
+
+}
---
+++ Model_Proiect3/ObjectWCF/InterfaceWCF.cs
@@ -0,0 +1,29 @@
+﻿using Model;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace ObjectWCF
+{
+    [ServiceContract]
+    interface InterfaceImages
+    {
+        [OperationContract]
+        void saveImage(string Name, string Path, string About, string Place, string People);
+        [OperationContract]
+        void UpdateImage1(string Name, string Path, string About, string Place, string People);
+        [OperationContract]
+        void deleteImage(string Path);
+        [OperationContract]
+        List<PhotosVideos> getPhotos();
+        [OperationContract]
+        List<Place> getPlaces();
+        [OperationContract]
+        List<Person> getPerson();
+
+    }
+
+    [ServiceContract]
+    interface IMplementInterface : InterfaceImages
+    {
+    }
+}
